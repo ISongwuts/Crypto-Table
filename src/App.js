be2c043a12f50e2b6ca07b11f08ca9f6624b1bcf
ICEACEs <icycaster41@gmail.com>
2021-11-26 20:11:56 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { useState, useEffect } from 'react'
+import EzReactTable from 'ez-react-table'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+    const [coins, setCoins] = useState([])
+
+    const fetchCoins = async () => {
+        try{
+            const res = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+        }catch(e){
+            alert("API Err")
+        }
+    }
+
   return (
     <div className="App">
       <header className="App-header">
